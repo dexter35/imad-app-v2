af5422dcc17f3df5a057dc44bae9a58ecd9fca4d
dexter35 <arpitch35@gmail.com>
2017-04-14 22:49:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,6 @@ function moveRight(){
     image.style.marginLeft=marginLeft+'px';
 }
 image.onclick=function(){
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
  // image.style.marginLeft='100px';  
 };
