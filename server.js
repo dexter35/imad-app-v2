9d2bb54bcecd2dbd7089a350a6925d7a5f824a11
dexter35 <arpitch35@gmail.com>
2017-04-14 21:34:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,9 +50,7 @@ function createTemplate(data){
 </html>`;
 return htmlTemplate;
 }
-var createTemplate={
-    
-};
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -65,17 +63,6 @@ app.get('/:articleName',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
