fbfe2a9e8cc713bfc59f89d5d882439e5982b102
dexter35 <arpitch35@gmail.com>
2017-04-14 22:44:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var x=document.getElementById('main-text');
 x.innerHTML="new value";
+
+var image=document.getElementById('img');
+image.onclick=function(){
+  image.style.marginLeft='100px';  
+};
