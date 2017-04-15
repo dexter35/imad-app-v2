9c070856bc3e38791512b947f34f08382a3012ae
dexter35 <arpitch35@gmail.com>
2017-04-15 22:59:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,7 @@ btn.onclick=function(){
   var request=new XMLHttpRequest();
   
   //capture the response and store it in a variable
+  
   request.onreadystatechange=function(){
     if(request.readyState===XMLHttpRequest.DONE){
         //take some action
@@ -21,11 +22,8 @@ btn.onclick=function(){
   //make the request
   request.open('GET','http://dexter35.imad.hasura-app.io/counter',true);
   request.send(null);
-  
   //render the variable in the correct span
-  
 };
-
 //submit name
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
@@ -40,7 +38,7 @@ submit.onclick=function(){
         //take some action
         if(request.status===200){       //request successfully competed;200
             var names=request.responseText;
-            names=JSON.parse(names);
+            names=JSON.parse(names); //converting the string back to the object or javascript array
             var list='';
             for(var i=0;i<names.length;i++){
                 list+='<li>'+names[i]+'</li>';
@@ -54,7 +52,7 @@ submit.onclick=function(){
   };
   
   //make the request
-  request.open('GET','http://dexter35.imad.hasura-app.io/subnit-name?name='+name,true);
+  request.open('GET','http://dexter35.imad.hasura-app.io/submit-name?name='+name,true);
   request.send(null);
   
     //capture a kist of names and render it as a list
