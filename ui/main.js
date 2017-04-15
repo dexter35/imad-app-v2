13bfd033af5a27f9729760d7b2c577f4e99b11ec
dexter35 <arpitch35@gmail.com>
2017-04-15 16:53:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,27 @@
 
 var btn=document.getElementById("counter");
-var counter=0;
+
 btn.onclick=function(){
-  //make a request
-  
+  //create the request object
+  var request=new XMLHttpRequest();
   
   //capture the response and store it in a variable
+  request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest,DONE){
+        //take some action
+        if(request.status===200){       //request successfully competed;200
+            var counter=request.responseText;       //extract value from request
+            var span=document.getElementById("count");
+            span.innerHTML=counter.toString();
+        }
+    }  
+    //not done yet
+  };
   
+  //make the request
+  request.open('GET','http://dexter35.imad.hasura-app.io/counter',true);
+  request.send(null);
   
   //render the variable in the correct span
-  counter=counter+1;
-  var span=document.getElementById("count");
-  span.innerHTML=counter.toString();
-    
+  
 };
