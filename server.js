acaa3c847fd240c0720705d215e7d7fd6afdb6ff
dexter35 <arpitch35@gmail.com>
2017-06-09 02:00:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg'.Pool);
 
+var configuration={
+    user:'dexter35',
+    database:'dexter35',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 
 var articles={
@@ -56,6 +64,21 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return a response with the reslts
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+            
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 var counter=0;
